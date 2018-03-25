59abc75265aebf2bca3e2de97af430c65bc0a61c
HoussineDMN <37777514+HoussineDMN@users.noreply.github.com>
2018-03-25 18:07:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,24 +6,16 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'BOT') {
-    	message.channel.send('HERE!');
-  	}
-});
-
-client.on('message', message => {
     if (message.content === 'ping') {
-    	message.reply('PONG! HAHAHAH MA BOI');
+    	message.channel.send('PONG!');
   	}
 });
 
 client.on('message', message => {
-    if (message.content === '??invite') {
-    	message.reply('https://discordapp.com/oauth2/authorize?client_id=427502556059795468&scope=bot&permissions=8');
+    if (message.content === 'bing') {
+    	message.reply('BONG!');
   	}
 });
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
-  	}
-});
