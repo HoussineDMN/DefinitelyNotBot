9586b3f42340463ad5b1cb057951c5a7ed1c309b
HoussineDMN <37777514+HoussineDMN@users.noreply.github.com>
2018-03-26 16:41:09 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,15 +13,9 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === '??invite') {
-    	message.reply('ttps://discordapp.com/oauth2/authorize?client_id=427502556059795468&scope=bot&permissions=8');
+    	message.reply('https://discordapp.com/oauth2/authorize?client_id=427502556059795468&scope=bot&permissions=8');
   	}
 });
-
-// THIS FOR ACTIVITY PLACE
-bot.user.setActivity("??help | in " + bot.guilds.size + " servers") 
-
-// GAME AND STREAMING
-bot.user.setGame('??invite') // Here for game statues testing.
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
